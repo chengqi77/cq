59d24290a6b97690dc6864fa124c36d85464121e
chengqi77 <2113265828@qq.com>
2019-05-10 14:52:15 +0800

    -

--- src/service/getData.js
+++ src/service/getData.js
@@ -3,13 +3,13 @@ import { IPcas, IPportal, fetch, post, put, postConfig } from './http.js';
 // export const BASICURL = IPadmin;
 // export const uploadPic = data => post('', data);
 
-// getUserInfo
+// 新建需求单
 export const demandCardSave = (data) => post('/demandCard/save', data);
 
-
+//需求列表详情
 export const getById = (id) => fetch(`/demandCard/getById?id=${id}`);
 // http://127.0.0.1:8082/out-user-sys-background
-
+//需求号搜索
 export const serialNumber = (id) => fetch(`/demandCard/getSerialNums?serialNumber=${id}`);
 
 export const getmemberAccount = (data) => fetch('member/find/agent/account', data);

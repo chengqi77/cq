1810bb202179f8640612d614491cd55b2ef2c922
chengqi77 <2113265828@qq.com>
2019-05-11 12:23:55 +0800

    -

--- src/components/listWrapper.vue
+++ src/components/listWrapper.vue
@@ -3,16 +3,9 @@
     <div v-show="showCustomLoading" class="custom-loading">
       <span>加载中...</span>
     </div>
-    <mt-loadmore
-      ref="loadmore"
-      :top-method="refresh"
-      v-infinite-scroll="loadmore"
-      infinite-scroll-disabled="loading"
-      infinite-scroll-distance="10"
-      @top-status-change="handleTopChange"
-      class="loadmore-box"
-    >
-      <ul>
+    <mt-loadmore ref="loadmore" :top-method="refresh"
+      @top-status-change="handleTopChange" class="loadmore-box">
+      <ul  v-infinite-scroll="loadmore" infinite-scroll-disabled="loading" infinite-scroll-distance="10">
         <slot name="list"></slot>
         <li v-show="status === 'success' && list.length ===0" class="no-data">暂无数据</li>
         <li v-show="status === 'loaded'" class="no-more">没有更多了</li>
@@ -25,112 +18,112 @@
   </div>
 </template>
 <script>
-/*
-1.没法控制loadmore的内部topStatus状态，只能通过下拉来触发
-2.下拉来派发更新数据的行为
-3.下拉的loading 通过 数据加载状态和topstatus共同控制，但是slot="top" 只有在触发下拉的动作才会显示
-4.所以第一次获取数据时 显示自定义loading
-*/
-import { delHtmlTag, delBrTag } from "@/utils";
-export default {
-  props: [
-    "requestData",
-    "onDataChange",
-    "className",
-    "allowRefresh",
-    "allowLoadmore"
-  ],
-  data() {
-    return {
-      list: [],
-      status: "init", //loading success error
-      topStatus: "pull",
-      current: 0
-    };
-  },
-  created() {
-    this.getData(1);
-  },
-  watch: {
-    status(nextStatus, prevStatus) {
-      //如果数据状态由加载改变 则关闭下拉动画
-      if (
-        prevStatus === "loading" &&
-        ["success", "error"].includes(nextStatus)
-      ) {
-        this.$refs.loadmore.onTopLoaded();
-      }
-    }
-  },
-  computed: {
-    showCustomLoading() {
-      const { status, topStatus } = this;
-      return status === "loading" && topStatus === "pull";
-    },
-    showLoading() {
-      const { status, topStatus } = this;
-      return status === "loading" || topStatus === "loading";
-    }
-  },
-  methods: {
-    getData(current) {
-      if (["loading", "loaded"].includes(this.status)){
-        this.$refs.loadmore.onTopLoaded();
-        return;
+  /*
+  1.没法控制loadmore的内部topStatus状态，只能通过下拉来触发
+  2.下拉来派发更新数据的行为
+  3.下拉的loading 通过 数据加载状态和topstatus共同控制，但是slot="top" 只有在触发下拉的动作才会显示
+  4.所以第一次获取数据时 显示自定义loading
+  */
+  import { delHtmlTag, delBrTag } from "@/utils";
+  export default {
+    props: [
+      "requestData",
+      "onDataChange",
+      "className",
+      "allowRefresh",
+      "allowLoadmore"
+    ],
+    data() {
+      return {
+        list: [],
+        status: "init", //loading success error
+        topStatus: "pull",
+        current: 0
       };
-      this.status = "loading";
-      this.requestData({ current })
-        .then(res => {
-          if (res.length === 0) {
-            this.status = "loaded";
-            return;
-          }
-          if (current === 1) {
-            this.list = res;
-          } else {
-            this.list = this.list.concat(res);
-          }
-          this.onDataChange(this.list);
-          this.status = "success";
-          this.current += 1;
-        })
-        .catch(e => {
-          this.status = "error";
-        });
     },
-    refresh() {
-      if (this.allowRefresh) {
-        this.getData(1);
-      } else {
-        this.$refs.loadmore.onTopLoaded();
+    created() {
+      this.getData(1);
+    },
+    watch: {
+      status(nextStatus, prevStatus) {
+        //如果数据状态由加载改变 则关闭下拉动画
+        if (
+          prevStatus === "loading" &&
+          ["success", "error"].includes(nextStatus)
+        ) {
+          this.$refs.loadmore.onTopLoaded();
+        }
       }
     },
-    loadmore(...v) {
-      this.allowLoadmore && this.getData(this.current + 1);
+    computed: {
+      showCustomLoading() {
+        const { status, topStatus } = this;
+        return status === "loading" && topStatus === "pull";
+      },
+      showLoading() {
+        const { status, topStatus } = this;
+        return status === "loading" || topStatus === "loading";
+      }
     },
-    handleTopChange(status) {
-      this.topStatus = status;
+    methods: {
+      getData(current, force = false) {
+        if (["loading", "loaded"].includes(this.status) && !force) {
+          this.$refs.loadmore.onTopLoaded();
+          return;
+        };
+        this.status = "loading";
+        this.requestData({ current })
+          .then(res => {
+            if (res.length === 0) {
+              this.status = "loaded";
+              return;
+            }
+            if (current === 1) {
+              this.list = res;
+            } else {
+              this.list = this.list.concat(res);
+            }
+            this.onDataChange(this.list);
+            this.status = "success";
+            this.current += 1;
+          })
+          .catch(e => {
+            this.status = "error";
+          });
+      },
+      refresh() {
+        if (this.allowRefresh) {
+          this.getData(1);
+        } else {
+          this.$refs.loadmore.onTopLoaded();
+        }
+      },
+      loadmore(...v) {
+        this.allowLoadmore && this.getData(this.current + 1);
+      },
+      handleTopChange(status) {
+        this.topStatus = status;
+      },
+      onchange() {
+        //  this.$router.push({ path:'/DemandDetails?id='+id.P})
+      }
     },
-    onchange() {
-      //  this.$router.push({ path:'/DemandDetails?id='+id.P})
-    }
-  },
-  components: {}
-};
+    components: {}
+  };
 </script>
 
 <style lang="less">
-.list-wrapper {
-  min-height: 200px;
-  .custom-loading {
-    text-align: center;
+  .list-wrapper {
+    min-height: 200px;
+    .custom-loading {
+      text-align: center;
+    }
+    .no-data,
+    .no-more {
+      text-align: center;
+      padding: 15px;
+      font-size: 14px;
+      color: #000;
+    }
   }
-  .no-data,
-  .no-more {
-    text-align: center;
-    padding: 15px;
-    font-size: 14px;
-    color: #000;
-  }
-}
 </style>
